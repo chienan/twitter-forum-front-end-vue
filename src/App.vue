fad588e79278b88fe00dc8d727a3676c39b52f1f
Ann Peng <boaimpact@gmail.com>
2020-12-15 16:45:00 +0800

    add style: Tweets & relevant components

--- src/App.vue
+++ src/App.vue
@@ -11,3 +11,82 @@ export default {
   name: "App"
 };
 </script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100;300;400;500;700;900&display=swap");
+
+/** CSS reset  **/
+body {
+  margin: 0;
+  padding: 0;
+}
+
+* {
+  box-sizing: border-box;
+  font-family: "Noto Sans TC", sans-serif;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+  margin: 0px;
+  padding: 0px;
+}
+
+ul,
+ol,
+li {
+  list-style: none;
+  margin: 0px;
+  padding: 0px;
+}
+
+a,
+a:hover,
+a:focus {
+  text-decoration: none;
+}
+
+img {
+  width: 100%;
+  height: 100%;
+  display: block;
+}
+
+.btn,
+button,
+button:focus,
+input:focus,
+textarea:focus,
+select,
+select:focus,
+a:focus {
+  outline: 0px !important;
+}
+
+button {
+  background-color: transparent;
+  border: 0;
+  cursor: pointer;
+}
+
+button[disabled] {
+  pointer-events: none;
+}
+
+input {
+  padding: 0;
+}
+
+input:required {
+  box-shadow: none;
+}
+
+input:invalid {
+  box-shadow: none;
+}
+</style>
--- src/components/MainTweets.vue
+++ src/components/MainTweets.vue
@@ -1,3 +1,446 @@
 <template>
-  <h1>TweetsList</h1>
+  <div class="container">
+    <div class="main-content">
+      <div class="main-header">
+        <div class="header-title">首頁</div>
+      </div>
+      <!-- tweet-input -->
+      <div class="tweet-input">
+        <div class="input-content">
+          <div class="input-content-top">
+            <!--user-image-->
+            <div class="user-image">
+              <div class="circle"></div>
+            </div>
+            <div class="user-input">有什麼新鮮事？</div>
+          </div>
+          <div class="input-content-bottom">
+            <button class="btn-tweet">推文</button>
+          </div>
+        </div>
+      </div>
+      <!-- tweet-list -->
+      <div class="tweet-list">
+        <!--tweet item start-->
+        <div class="tweet-item">
+          <a href class="item-left">
+            <div class="circle"></div>
+          </a>
+
+          <div class="item-right">
+            <div class="item-user-info d-flex">
+              <a href class="user-name">Apple</a>
+              <!-- <div class="user-name">Apple</div> -->
+              <a href class="user-id">@apple</a>
+              <!-- <div class="user-id">@apple</div> -->
+
+              <div class="time">・3hrs</div>
+            </div>
+            <a
+              href
+              class="item-content"
+            >Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum.Nulla Lorem mollit cupidatat irure.</a>
+            <div class="item-interaction">
+              <a href class="tweet-reply">
+                <img src="https://i.imgur.com/I3DHrNy.png" id="icon-reply" alt />
+                <p>13</p>
+              </a>
+              <!-- <div class="tweet-reply">
+              <img src="https://i.imgur.com/I3DHrNy.png" id="icon-reply" alt />
+              <p>13</p>
+              </div>-->
+              <a href class="tweet-like">
+                <img src="https://i.imgur.com/gCFSWst.png" id="icon-like" alt />
+                <p>5</p>
+              </a>
+            </div>
+          </div>
+        </div>
+        <!-- tweet-item-test -->
+        <div class="tweet-item">
+          <a href class="item-left">
+            <div class="circle"></div>
+          </a>
+
+          <div class="item-right">
+            <div class="item-user-info d-flex">
+              <a href class="user-name">Apple</a>
+              <!-- <div class="user-name">Apple</div> -->
+              <a href class="user-id">@apple</a>
+              <!-- <div class="user-id">@apple</div> -->
+
+              <div class="time">・3hrs</div>
+            </div>
+            <a
+              href
+              class="item-content"
+            >Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum.Nulla Lorem mollit cupidatat irure.</a>
+            <div class="item-interaction">
+              <a href class="tweet-reply">
+                <img src="https://i.imgur.com/I3DHrNy.png" id="icon-reply" alt />
+                <p>13</p>
+              </a>
+              <!-- <div class="tweet-reply">
+              <img src="https://i.imgur.com/I3DHrNy.png" id="icon-reply" alt />
+              <p>13</p>
+              </div>-->
+              <a href class="tweet-like">
+                <img src="https://i.imgur.com/gCFSWst.png" id="icon-like" alt />
+                <p>5</p>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="tweet-item">
+          <a href class="item-left">
+            <div class="circle"></div>
+          </a>
+
+          <div class="item-right">
+            <div class="item-user-info d-flex">
+              <a href class="user-name">Apple</a>
+              <!-- <div class="user-name">Apple</div> -->
+              <a href class="user-id">@apple</a>
+              <!-- <div class="user-id">@apple</div> -->
+
+              <div class="time">・3hrs</div>
+            </div>
+            <a
+              href
+              class="item-content"
+            >Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum.Nulla Lorem mollit cupidatat irure.</a>
+            <div class="item-interaction">
+              <a href class="tweet-reply">
+                <img src="https://i.imgur.com/I3DHrNy.png" id="icon-reply" alt />
+                <p>13</p>
+              </a>
+              <!-- <div class="tweet-reply">
+              <img src="https://i.imgur.com/I3DHrNy.png" id="icon-reply" alt />
+              <p>13</p>
+              </div>-->
+              <a href class="tweet-like">
+                <img src="https://i.imgur.com/gCFSWst.png" id="icon-like" alt />
+                <p>5</p>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="tweet-item">
+          <a href class="item-left">
+            <div class="circle"></div>
+          </a>
+
+          <div class="item-right">
+            <div class="item-user-info d-flex">
+              <a href class="user-name">Apple</a>
+              <!-- <div class="user-name">Apple</div> -->
+              <a href class="user-id">@apple</a>
+              <!-- <div class="user-id">@apple</div> -->
+
+              <div class="time">・3hrs</div>
+            </div>
+            <a
+              href
+              class="item-content"
+            >Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum.Nulla Lorem mollit cupidatat irure.</a>
+            <div class="item-interaction">
+              <a href class="tweet-reply">
+                <img src="https://i.imgur.com/I3DHrNy.png" id="icon-reply" alt />
+                <p>13</p>
+              </a>
+              <!-- <div class="tweet-reply">
+              <img src="https://i.imgur.com/I3DHrNy.png" id="icon-reply" alt />
+              <p>13</p>
+              </div>-->
+              <a href class="tweet-like">
+                <img src="https://i.imgur.com/gCFSWst.png" id="icon-like" alt />
+                <p>5</p>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="tweet-item">
+          <a href class="item-left">
+            <div class="circle"></div>
+          </a>
+
+          <div class="item-right">
+            <div class="item-user-info d-flex">
+              <a href class="user-name">Apple</a>
+              <!-- <div class="user-name">Apple</div> -->
+              <a href class="user-id">@apple</a>
+              <!-- <div class="user-id">@apple</div> -->
+
+              <div class="time">・3hrs</div>
+            </div>
+            <a
+              href
+              class="item-content"
+            >Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum.Nulla Lorem mollit cupidatat irure.</a>
+            <div class="item-interaction">
+              <a href class="tweet-reply">
+                <img src="https://i.imgur.com/I3DHrNy.png" id="icon-reply" alt />
+                <p>13</p>
+              </a>
+              <!-- <div class="tweet-reply">
+              <img src="https://i.imgur.com/I3DHrNy.png" id="icon-reply" alt />
+              <p>13</p>
+              </div>-->
+              <a href class="tweet-like">
+                <img src="https://i.imgur.com/gCFSWst.png" id="icon-like" alt />
+                <p>5</p>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="tweet-item">
+          <a href class="item-left">
+            <div class="circle"></div>
+          </a>
+
+          <div class="item-right">
+            <div class="item-user-info d-flex">
+              <a href class="user-name">Apple</a>
+              <!-- <div class="user-name">Apple</div> -->
+              <a href class="user-id">@apple</a>
+              <!-- <div class="user-id">@apple</div> -->
+
+              <div class="time">・3hrs</div>
+            </div>
+            <a
+              href
+              class="item-content"
+            >Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum.Nulla Lorem mollit cupidatat irure.</a>
+            <div class="item-interaction">
+              <a href class="tweet-reply">
+                <img src="https://i.imgur.com/I3DHrNy.png" id="icon-reply" alt />
+                <p>13</p>
+              </a>
+              <!-- <div class="tweet-reply">
+              <img src="https://i.imgur.com/I3DHrNy.png" id="icon-reply" alt />
+              <p>13</p>
+              </div>-->
+              <a href class="tweet-like">
+                <img src="https://i.imgur.com/gCFSWst.png" id="icon-like" alt />
+                <p>5</p>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="tweet-item">
+          <a href class="item-left">
+            <div class="circle"></div>
+          </a>
+
+          <div class="item-right">
+            <div class="item-user-info d-flex">
+              <a href class="user-name">Apple</a>
+              <!-- <div class="user-name">Apple</div> -->
+              <a href class="user-id">@apple</a>
+              <!-- <div class="user-id">@apple</div> -->
+
+              <div class="time">・3hrs</div>
+            </div>
+            <a
+              href
+              class="item-content"
+            >Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum.Nulla Lorem mollit cupidatat irure.</a>
+            <div class="item-interaction">
+              <a href class="tweet-reply">
+                <img src="https://i.imgur.com/I3DHrNy.png" id="icon-reply" alt />
+                <p>13</p>
+              </a>
+              <!-- <div class="tweet-reply">
+              <img src="https://i.imgur.com/I3DHrNy.png" id="icon-reply" alt />
+              <p>13</p>
+              </div>-->
+              <a href class="tweet-like">
+                <img src="https://i.imgur.com/gCFSWst.png" id="icon-like" alt />
+                <p>5</p>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+
+
+<style scoped>
+.container {
+  position: relative;
+}
+
+.main-content {
+  height: 100%;
+  width: 600px;
+  position: absolute;
+  left: 300px;
+}
+
+.main-header {
+  width: 600px;
+  height: 55px;
+  background: #ffffff;
+  border: 1px solid #e6ecf0;
+  border-bottom: none;
+  box-sizing: border-box;
+}
+
+.header-title {
+  height: 100%;
+  margin: 15px 15px 15px;
+  font-weight: 550;
+  font-size: 18px;
+  line-height: 26px;
+  color: #1c1c1c;
+}
+
+.tweet-input {
+  width: 600px;
+  height: 120px;
+  background: #ffffff;
+  border: 1px solid #e6ecf0;
+  border-bottom: 10px solid #e6ecf0;
+  box-sizing: border-box;
+}
+
+.input-content {
+  /* border: 1px solid gray; */
+  box-sizing: border-box;
+  height: 100%;
+}
+
+.input-content-top {
+  height: 50%;
+  display: flex;
+}
+
+.user-image {
+  height: 100%;
+  width: 10%;
+  /* border: 1px solid gray; */
+}
+
+.circle {
+  background: #c4c4c4;
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  margin: 9px 10px auto 15px;
+}
+
+.user-input {
+  height: 100%;
+  width: 90%;
+  /* border: 1px solid gray; */
+  padding: 20px 15px 10px;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 26px;
+  color: #9197a3;
+  letter-spacing: 0.5px;
+}
+
+.input-content-bottom {
+  position: relative;
+  height: 50%;
+  /* border: 1px solid gray; */
+}
+
+.btn-tweet {
+  position: absolute;
+  right: 15px;
+  bottom: 10px;
+  width: 64px;
+  height: 40px;
+  background: #ff6600;
+  border-radius: 100px;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 18px;
+  color: #ffffff;
+}
+
+.tweet-item {
+  border: 1px solid #e6ecf0;
+  background: #ffffff;
+  border-top: none;
+  display: flex;
+  flex-direction: row;
+}
+
+.item-user-info {
+  margin: 5px auto 0px auto;
+}
+
+.user-name {
+  font-weight: bold;
+  font-size: 15px;
+  line-height: 22px;
+  color: #1c1c1c;
+  order: 0;
+  flex-grow: 0;
+  margin: 5px 5px 5px 0px;
+}
+
+.user-id,
+.time {
+  font-weight: 400;
+  font-size: 15px;
+  line-height: 22px;
+  color: #657786;
+  flex: none;
+  order: 1;
+  flex-grow: 0;
+  margin: 5px 0px;
+}
+
+.item-content {
+  font-weight: 400;
+  font-size: 15px;
+  line-height: 22px;
+  color: #1c1c1c;
+}
+
+.item-interaction {
+  margin: 12px 0px;
+}
+
+.item-interaction,
+.tweet-reply,
+.tweet-like {
+  display: flex;
+  vertical-align: middle;
+}
+
+.tweet-reply,
+.tweet-like {
+  font-weight: 400;
+  font-size: 13px;
+  line-height: 13px;
+  color: #657786;
+}
+
+.tweet-reply {
+  width: 20%;
+}
+
+.tweet-like {
+  width: 80%;
+}
+
+#icon-reply {
+  width: 12.34px;
+  height: 12.34px;
+  margin-right: 11.35px;
+}
+
+#icon-like {
+  height: 11.82px;
+  width: 12.56px;
+  margin-right: 11.35px;
+}
+</style>
