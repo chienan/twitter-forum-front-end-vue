18907438d276a37b1ed555c0735198bc152a4b1c
Ann Peng <boaimpact@gmail.com>
2020-12-21 16:44:39 +0800

    feat: resolve conflict

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import NotFound from '../views/NotFound'
 import Tweets from '../views/Tweets'
-<<<<<<< HEAD
 import UserProfile from '../views/UserProfile'
 import UserFollower from '../views/UserFollower'
 import UserFollowing from '../views/UserFollowing'
@@ -11,7 +10,7 @@ import UserProfileLiked from '../views/UserProfileLiked'
 import UserOtherProfile from '../views/UserOtherProfile'
 import UserOtherReplied from '../views/UserOtherReplied'
 import UserOtherLiked from '../views/UserOtherLiked'
->>>>>>> 49f4ad3... update UserFollowing & UserFollower
+
 
 
 Vue.use(Router)
@@ -75,43 +74,36 @@ export default new Router({
       name: 'user-other-liked',
       component: UserOtherLiked
     },
-
-
-
     {
-<<<<<<< HEAD
-=======
       path: '/tweet/:id',
       name: 'tweet',
       component: () => import('../views/Tweet.vue')
-
     },
     {
-<<<<<<< HEAD
+
       path: '/users/:id/edit',
       name: 'users-edit',
       //動態載入
       component: () => import('../views/UserProfileEdit.vue')
-=======
+    },
+    {
+
       path: '/users/follower',
       name: 'user-follower',
       component: UserFollower
->>>>>>> 49f4ad3... update UserFollowing & UserFollower
     },
     {
-<<<<<<< HEAD
       path: '/admin',
       name: 'admin-signIn',
       component: () => import('../views/AdminSignIn.vue')
-=======
+    },
+    {
       path: '/users/following',
       name: 'user-following',
       component: UserFollowing
->>>>>>> 49f4ad3... update UserFollowing & UserFollower
     },
 
     {
->>>>>>> f920617... create UserProfileEdit
       path: '*',
       name: 'not-found',
       component: NotFound
